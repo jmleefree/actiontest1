1a04d612a7da7993a242c8c05dd92a19e148f8c3
ipns2014 <ipns2014@gmail.com>
2021-03-26 10:15:27 +0900

    Update code2

--- main.go
+++ main.go
@@ -2,14 +2,18 @@ package main
 
 import "fmt"
 
+// test
 func Sum(x int, y int) int {
 	return x + y
 }
 
+// test
 func Multiply(x int, y int) int {
 	return x * y
 }
 
 func main() {
 	fmt.Printf("result : %d\n", Sum(5, 6))
+	game_version := 3
+	fmt.Printf("Super Mario %s\n", game_version)
 }
