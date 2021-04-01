86bac66ded12aa3acad0701a484af8d5b7059bc9
ipns2014 <ipns2014@gmail.com>
2021-04-01 10:05:46 +0900

    update 33

--- main_test.go
+++ main_test.go
@@ -22,3 +22,10 @@ func TestSum3(t *testing.T) {
 		t.Errorf("Sum3 was incorrect, got: %d, want: %d.", total, 10)
 	}
 }
+
+func TestSum4(t *testing.T) {
+	total := Sum(5, 5)
+	if total != 10 {
+		t.Errorf("Sum4 was incorrect, got: %d, want: %d.", total, 10)
+	}
+}
