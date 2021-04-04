8f118a77e072f3d2e99c7a367efcdb30c6feccdc
ipns2014 <ipns2014@gmail.com>
2021-04-04 14:42:04 +0900

    update 368

--- main.go
+++ main.go
@@ -1,7 +1,5 @@
 package main
-
 import "fmt"
-
 func Sum(x int, y int) int {
 	return x + y
 }
@@ -14,12 +12,11 @@ func Sub2(x int, y int) int {
 	return x - y
 }
 
-func Sub3(x int, y int, z int) int 
-{
+func Sub3(x int, y int, z int) int {
 	return x - y
 }
 
-func main() 
-{
+func main() {
+	test :=3
 	fmt.Printf("result : %d\n", Sum(5, 6))
 }
